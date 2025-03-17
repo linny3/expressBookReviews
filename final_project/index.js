212ae65c0fb62b88134298b8ec16df02b53913ab
linny3 <ryan.lindberg3@gmail.com>
2025-03-17 11:06:28 -0400

    added / route

--- final_project/index.js
+++ final_project/index.js
@@ -5,35 +5,6 @@ const { authenticated } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
-let users = []
-
-// Check if a user with the given username already exists
-const doesExist = (username) => {
-    // Filter the users array for any use with the same username
-    let userswithsamename = users.filter((user) => {
-        return user.username === username;
-    });
-    // Return true if any user with the same username is found, other false
-    if (userswithsamename.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// Check if the user with the given username and password exists
-const authenticatedUser = (username, password) => {
-    // Filter the users array for any user with the same username and password
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
 const app = express();
 
@@ -60,51 +31,8 @@ app.use("/customer/auth/*", function auth(req,res,next){
     }
 });
 
-app.post("/login", (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    // Check if username or password is missing
-    if (!username || !password) {
-        return res .status(404).json({message: "Error logging in"});
-    }
 
-    // Authenitcate user
-    if (authenticatedUser(username,password)) {
-        // Generate JWT access token
-        let accessToken = jwt.sign({
-            data: password
-        }, 'access', {expiresIn: 60 * 60});
-
-        // Store access token and username in session
-        req.session.authorization = {
-            accessToken, username
-        }
-        return res.status(200).send("User successfully logged in");
-    } else {
-        return res.status(208).json({message: "Invalid Login. Check username and password"});
-    }
-});
-
-// Register a new user
-app.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Check if both username and password are provided
-    if (username && password) {
-        // Check if the user does not already exist
-        if (!doesExist(username)) {
-            //Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});
-        }
-    }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
-});
  
 const PORT =5000;
 
