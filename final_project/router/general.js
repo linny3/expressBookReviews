21cef8a08c9cdeb9976fc18c176e1295a877cc6b
linny3 <ryan.lindberg3@gmail.com>
2025-03-22 13:04:25 -0400

    Finished node.js and express final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,12 +30,58 @@ public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books, null, 4));
   
 });
+// Get the book list using promises
+public_users.get('/books/', function (req,res) {
+    // Create promise for getting books
+    const get_books = new Promise((resolve, reject) => {
+        resolve("Books Found");
+    })
+    // Call get_books promise
+    get_books
+    // If resolved 
+    .then((message) => {
+        console.log(message);
+        res.send(JSON.stringify(books, null, 4));
+    })
+    // If error
+    .catch(() => {
+        console.log("Books not found");
+        return res.status(404).json("Books Not Found");
+    })
+});
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   res.send(books[isbn])
 
+ });
+
+ // Get book detail based on ISBN using promises
+public_users.get('/books/isbn/:isbn', function (req,res) {
+    // Get isbn number
+    const isbn = req.params.isbn;
+    // New Promise for getting book
+    const bookISBN = new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve("Book found");
+        } else {
+            reject("Book not found");
+        }
+    });
+    // Call book promise
+    bookISBN
+    // If promise successful
+    .then((message) => {
+        console.log(message);
+        res.send(books[isbn]);
+    })
+    // If promise failed
+    .catch((error) => {
+        console.log(error);
+        res.status(404).json(error);
+    });
  });
   
 // Get book details based on author
@@ -55,6 +101,43 @@ public_users.get('/author/:author',function (req, res) {
 
 });
 
+// Get book detail based on author using promises
+public_users.get('/books/author/:author', function(req, res) {
+    let authorsBooks = [];
+    const booksByAuthor = new Promise((resolve,reject) => {
+        
+        // Get author
+        const author = req.params.author;
+        // Get keys for books array
+        const isbns = Object.keys(books);
+        // Iterate through books and get each book for author adding it to booksByAuthor
+        isbns.forEach((isbn) => {
+            if(books[isbn].author === author) {
+                authorsBooks.push({"isbn":isbn, "title":books[isbn]["title"], "review":books[isbn]["review"]});
+            }
+        });
+        // If books found for author resolve promise else reject promise
+        if (authorsBooks.length > 0) {
+            resolve("Books by author found");
+        } else {
+            reject("No books found for author");
+        }
+    });
+
+    booksByAuthor
+    // if books were found
+    .then((message) => {
+        console.log(message);
+        res.send(JSON.stringify({authorsBooks}, null, 4));
+    })
+    // if books weren't found
+    .catch((error) => {
+        console.log(error);
+        res.status(404).json(error)
+
+    });
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     // Get book title
@@ -67,6 +150,38 @@ public_users.get('/title/:title',function (req, res) {
             res.send(books[isbn])
         }
     })
+});
+
+// Get all books based on title using promises
+public_users.get('/books/title/:title', function (req,res) {
+    // Get book title
+    let title = req.params.title;
+    // Get keys for books array
+    const isbns = Object.keys(books);
+    // Create books by title promise
+    const booksByTitle = new Promise((resolve, reject) => {
+        // Iterate through books and get title of the requested book and list book
+        isbns.forEach((isbn) => {
+            if(books[isbn].title === title) {
+                resolve("Book found")
+                title = books[isbn]
+            } else {
+                reject("No books under that title")
+            };
+        });
+    });
+    
+    booksByTitle
+    // If promise resolved
+    .then((message) => {
+        console.log(message);
+        res.send(title);
+    })
+    // If promise not resolved
+    .catch((error) => {
+        console.log(error);
+        res.status(404).json(error);
+    });
 });
 
 //  Get book review
